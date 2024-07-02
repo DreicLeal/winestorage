f2836b5e4d1be6508252b31450f286e886c919c2
unknown <dreicleal@hotmail.com>
2024-07-02 21:28:49 +0100

    Header Component
    Header component included

---
+++ wine-storage/src/components/Header.tsx
@@ -0,0 +1,10 @@
+import { HStack, Text } from "native-base";
+
+export const Header = () => {
+  return (
+    <HStack bgColor={"gray.600"} pt={16} pb={5} px={8} alignItems={"center"}>
+      <Text>Wines StockFlow and suply</Text>
+    </HStack>
+    
+  );
+};
