2e9b83eb2f0a8857e730d5bc525f0d4bb6a658d8
unknown <dreicleal@hotmail.com>
2024-09-02 09:53:45 +0100

    Wines Form improved
    Wines form Indentation and design improved

--- wine-storage/src/screens/AddWinesform.tsx
+++ wine-storage/src/screens/AddWinesform.tsx
@@ -10,6 +10,7 @@ import { Button } from "@components/Button";
 import { useNavigation } from "@react-navigation/native";
 import { AppNavigatorRoutesProp } from "src/routes/app.routes";
 import { WineDTO } from "src/dtos/WineDTO";
+import { Platform, KeyboardAvoidingView } from "react-native";
 
 const supplierSchema = yup.object().shape({
   company: yup.string().required("Campo Obrigatório"),
@@ -42,7 +43,7 @@ export const AddWinesForm = () => {
       name: "",
       region: "",
       type: "",
-      storage: undefined, 
+      storage: undefined,
       supplier: {
         company: "",
         seller: "",
@@ -59,105 +60,109 @@ export const AddWinesForm = () => {
   return (
     <VStack>
       <Header />
-      <ScrollView>
-        <VStack flex={1} px={10}>
-          <Center>
-            <Heading marginBottom={2}>Adicione um vinho</Heading>
-          </Center>
-          <Controller
-            control={control}
-            name="name"
-            render={({ field: { onChange, value } }) => (
-              <Input
-                placeholder="Nome"
-                onChangeText={onChange}
-                value={value}
-                errorMessage={errors.name?.message}
-              />
-            )}
-          />
-          <Controller
-            control={control}
-            name="region"
-            render={({ field: { onChange, value } }) => (
-              <Input
-                placeholder="Região"
-                onChangeText={onChange}
-                value={value}
-                errorMessage={errors.region?.message}
-              />
-            )}
-          />
-          <Controller
-            control={control}
-            name="type"
-            render={({ field: { onChange, value } }) => (
-              <Input
-                placeholder="Tipo"
-                onChangeText={onChange}
-                value={value}
-                errorMessage={errors.type?.message}
-              />
-            )}
-          />
-          <Controller
-            control={control}
-            name="storage"
-            render={({ field: { onChange, value } }) => (
-              <Input
-                placeholder="Quantidade"
-                onChangeText={(text) => onChange(Number(text))}
-                value={value !== undefined ? String(value) : ""}
-                keyboardType="numeric"
-                errorMessage={errors.storage?.message}
-              />
-            )}
-          />
-          <Controller
-            control={control}
-            name="supplier.company"
-            render={({ field: { onChange, value } }) => (
-              <Input
-                placeholder="Empresa"
-                onChangeText={onChange}
-                value={value}
-                errorMessage={errors.supplier?.company?.message}
-              />
-            )}
-          />
-          <Controller
-            control={control}
-            name="supplier.seller"
-            render={({ field: { onChange, value } }) => (
-              <Input
-                placeholder="Representante"
-                onChangeText={onChange}
-                value={value}
-                errorMessage={errors.supplier?.seller?.message}
-              />
-            )}
-          />
-          <Controller
-            control={control}
-            name="supplier.contact"
-            render={({ field: { onChange, value } }) => (
-              <Input
-                placeholder="Contato"
-                onChangeText={onChange}
-                value={value}
-                errorMessage={errors.supplier?.contact?.message}
-              />
-            )}
-          />
-          <Button
-            title="Adicionar"
-            variant={"solid"}
-            onPress={handleSubmit(onSubmit)}
-            height={16}
-            marginBottom={2}
-          />
-        </VStack>
-      </ScrollView>
+
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ flexGrow: 1, justifyContent: 'center', paddingBottom: 72 }}
+        >
+          <VStack space={2} px={10}>
+            <Center>
+              <Heading marginBottom={2}>Adicione um vinho</Heading>
+            </Center>
+            <Controller
+              control={control}
+              name="name"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="Nome"
+                  onChangeText={onChange}
+                  value={value}
+                  errorMessage={errors.name?.message}
+                />
+              )}
+            />
+            <Controller
+              control={control}
+              name="region"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="Região"
+                  onChangeText={onChange}
+                  value={value}
+                  errorMessage={errors.region?.message}
+                />
+              )}
+            />
+            <Controller
+              control={control}
+              name="type"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="Tipo"
+                  onChangeText={onChange}
+                  value={value}
+                  errorMessage={errors.type?.message}
+                />
+              )}
+            />
+            <Controller
+              control={control}
+              name="storage"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="Quantidade"
+                  onChangeText={(text) => onChange(Number(text))}
+                  value={value !== undefined ? String(value) : ""}
+                  keyboardType="numeric"
+                  errorMessage={errors.storage?.message}
+                />
+              )}
+            />
+            <Controller
+              control={control}
+              name="supplier.company"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="Empresa"
+                  onChangeText={onChange}
+                  value={value}
+                  errorMessage={errors.supplier?.company?.message}
+                />
+              )}
+            />
+            <Controller
+              control={control}
+              name="supplier.seller"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="Representante"
+                  onChangeText={onChange}
+                  value={value}
+                  errorMessage={errors.supplier?.seller?.message}
+                />
+              )}
+            />
+            <Controller
+              control={control}
+              name="supplier.contact"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="Contato"
+                  onChangeText={onChange}
+                  value={value}
+                  errorMessage={errors.supplier?.contact?.message}
+                />
+              )}
+            />
+            <Button
+              title="Adicionar"
+              variant={"solid"}
+              onPress={handleSubmit(onSubmit)}
+              height={16}
+              marginBottom={2}
+            />
+          </VStack>
+        </ScrollView>
     </VStack>
   );
 };
